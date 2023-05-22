a0c7cf8de248118528a82769608ef2f984d7c533
yudhaeric <yudhaericpamungkas@gmail.com>
2023-05-22 23:57:36 +0700

    Update Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,17 +9,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <style>
+    body {
+        background-color: #FFEEB3;
+    }
+
     .login-box {
-        border: solid 1px;
-        width: 500px;
+        background-color: #fff;
+        width: 400px;
         padding: 20px;
         box-sizing: border-box;
+        border-radius: 20px;
     }
 </style>
 <body>
     <div class="vh-100 d-flex justify-content-center align-items-center flex-column">
         @if (Session::has('status'))
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-danger vw-50" role="alert">
                 {{Session::get('message')}}
             </div>
         @endif
@@ -30,11 +35,11 @@
                     <label for="email" class="form-label">Email</label>
                     <input type="email" name="email" id="email" class="form-control" required>
                 </div>
-                <div class="mb-3">
+                <div class="mb-4">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" name="password" id="password" class="form-control" required>
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 w-50 mx-auto">
                     <button class="btn btn-primary form-control" type="submit">Login</button>
                 </div>
             </form>
